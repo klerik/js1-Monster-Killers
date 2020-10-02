ef188086dbe1a8483d5d40d1dcc26cf1556b1514
okuznietsov <okuznitsov@teladoc.com>
2020-10-02 15:37:08 +0300

    Add

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -59,7 +59,7 @@ function writeToLog (event, value, monsterHealth, playerHealth) {
 function reset () {
     currentMonsterHealth = chosenMaxLife;
     currentPlayerHealth = chosenMaxLife;
-    resetGame();
+    resetGame(chosenMaxLife);
 }
 
 function endRound () {
@@ -108,20 +108,14 @@ function endRound () {
     }
 
     if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
-        reset(chosenMaxLife);
+        reset();
     }
 }
 
 function attackMonster (mode) {
-    let maxDamage;
-    let logEvent;
-    if (mode === MODE_ATTACK) {
-        maxDamage = ATTACK_VALUE;
-        logEvent = LOG_EVENT_PLAYER_ATTACK;
-    } else if (mode === MODE_STRONG_ATTACK) {
-        maxDamage = STRONG_ATTACK_VALUE;
-        logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
-    }
+    const maxDamage = mode === MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
+    const logEvent = mode === MODE_ATTACK ? LOG_EVENT_PLAYER_ATTACK : LOG_EVENT_PLAYER_STRONG_ATTACK;
+
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
     writeToLog(
